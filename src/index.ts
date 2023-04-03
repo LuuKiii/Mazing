d944efa9a2f93d0c090e255f569b7a828b7facaa
Łukasz Kurdziel <lukasz.kurdziel99@gmail.com>
2023-04-03 14:53:19 +0200

    init Project refactor

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { App } from './app/main'
+import "./scss/main.scss"
+
+function importAssetGroup(r: any) {
+  let images: { [key: string]: any } = {};
+  r.keys().map((item: any, index: any) => {
+    images[item.replace('./', '')] = r(item);
+  })
+  return images;
+}
+
+const images = importAssetGroup(require.context('./assets', false, /\.(eot|svg|png|jpg|gif|ico)$/i));
+
+const app = new App();
